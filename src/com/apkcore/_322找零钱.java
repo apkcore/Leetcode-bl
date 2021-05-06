99e82b2c2b416d970a37c2e980d0eb4ea6baa1f3
chenhao <chenhao@wavewisdom.com>
2021-05-06 14:11:57 +0800

    feat(322数零钱):322数零钱

--- src/com/apkcore/_322找零钱.java
+++ src/com/apkcore/_322找零钱.java
@@ -50,6 +50,20 @@ public class _322找零钱 {
         System.out.println(new _322找零钱().coinChange(arr, 6249));
     }
 
+    /**
+     * 完全背包问题——填满容量为amount的背包最少需要多少硬币
+     *
+     * dp[j]代表含义：填满容量为j的背包最少需要多少硬币
+     * 初始化dp数组：因为硬币的数量一定不会超过amount，而amount <= 10^410
+     * 4
+     *  ，因此初始化数组值为10001；dp[0] = 0
+     * 转移方程：dp[j] = min(dp[j], dp[j - coin] + 1)
+     * 当前填满容量j最少需要的硬币 = min( 之前填满容量j最少需要的硬币, 填满容量 j - coin 需要的硬币 + 1个当前硬币）
+     * 返回dp[amount]，如果dp[amount]的值为10001没有变过，说明找不到硬币组合，返回-1
+     *
+     * 链接：https://leetcode-cn.com/problems/coin-change/solution/322-ling-qian-dui-huan-dong-tai-gui-hua-e2nt7/
+     * 来源：力扣（LeetCode）
+     */
     public int coinChange(int[] coins, int amount) {
         // 自底向上的动态规划
         if (amount < 0) {
